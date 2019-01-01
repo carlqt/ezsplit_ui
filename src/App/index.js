ea58024ecaab4ecc6d7ce36a4207f99d3a785dc0
Carl Tablante <carl.q.tablante@gmail.com>
2019-01-01 19:52:24 +0800

    Login page

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Login from 'App/login';
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <div />
+        <Route path="/login" component={Login} />
       </Router>
     )
   }
