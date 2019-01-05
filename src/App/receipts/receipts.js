9c21606870acca15162384db531ad4d3d50238cf
Carl Tablante <carl.q.tablante@gmail.com>
2019-01-05 21:01:15 +0800

    Receipt form fields

--- src/App/receipts/receipts.js
+++ src/App/receipts/receipts.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -12,6 +10,20 @@ import styles from './styles';
 
 
 class Receipts extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      receipt: {
+        description: '',
+        total: '',
+        members: [],
+        items: [],
+      },
+      step: 1,
+    }
+  }
+
   componentDidMount() {
     const { getMembers, computedMatch } = this.props;
     const homeID = computedMatch.params.id;
@@ -19,16 +31,58 @@ class Receipts extends Component {
     getMembers(homeID);
   }
 
-  renderMembers = ({email, id}) => {
+  receiptOnChange = (event) => {
+    const { name, value } = event.target;
+    const { receipt } = this.state;
+    receipt[name] = value;
 
+    this.setState({ receipt });
+  }
+
+  onCheckboxChange = (event, checked) => {
+    const { value } = event.target;
+
+    if (checked) {
+      this.insertMemberState(value);
+    } else {
+      this.removeMemberState(value);
+    }
+  }
+
+  insertMemberState = (id) => {
+    const { receipt } = this.state;
+    const { members } = this.props;
+
+    const member = members.find(mem => mem.id === id);
+
+    if (member) {
+      receipt.members.push(member);
+      this.setState({ receipt });
+    }
+  }
+
+  removeMemberState = (id) => {
+    const { receipt } = this.state;
+    const members = receipt.members
+
+    const index = members.findIndex(mem => mem.id === id);
+
+    if (index >= 0) {
+      receipt.members.splice(index, 1);
+      this.setState({ receipt });
+    }
+  }
+
+  renderMembers = ({email, id}) => {
     return(
       <FormControlLabel
         key={id}
         label={email}
         control={
           <Checkbox
-            value={1}
+            value={id.toString()}
             color="primary"
+            onChange={this.onCheckboxChange}
           />
         }
       />
@@ -37,26 +91,59 @@ class Receipts extends Component {
 
   render() {
     const { classes, members } = this.props;
+    const {
+      description,
+      total,
+    } = this.state.receipt;
 
     return (
       <div className={classes.root}>
         <Sidebar>
           <div className={classes.container}>
-            <TextField
-              className={classes.input}
-              name="Receipt Name"
-              label="Receipt Name"
-            />
-            <TextField
-              className={classes.input}
-              name="cost"
-              label="Total Cost"
-              type="number"
-            />
+            <div className={classes.formContainer}>
+              <TextField
+                className={classes.input}
+                name="description"
+                label="Receipt Name"
+                onChange={this.receiptOnChange}
+                value={description}
+              />
+              <TextField
+                className={classes.input}
+                name="total"
+                label="Total Cost"
+                type="number"
+                onChange={this.receiptOnChange}
+                value={total}
+              />
 
-            { members.map(this.renderMembers) }
+              { members.map(this.renderMembers) }
 
-            <Button variant="contained" color="primary">Next</Button>
+              <Button variant="contained" color="primary">Next</Button>
+            </div>
+
+            <div className={classes.formContainer}>
+              <div className={classes.menuItem}>
+                <TextField
+                  className={classes.input}
+                  name="Name"
+                  label="Name"
+                />
+                <TextField
+                  className={classes.input}
+                  name="price"
+                  label="Price"
+                  type="number"
+                />
+                <TextField
+                  className={classes.input}
+                  name="quantity"
+                  label="Quantity"
+                  type="number"
+                />
+              </div>
+              <Button variant="contained" color="primary">Create</Button>
+            </div>
           </div>
         </Sidebar>
       </div>
--- src/App/receipts/styles.js
+++ src/App/receipts/styles.js
@@ -1,13 +1,26 @@
 export default theme => ({
   root: {
     height: '100%',
+    padding: 10,
   },
   container: {
     display: 'flex',
+    height: '100%',
+    width: '100%',
+  },
+  formContainer: {
+    display: 'flex',
     flexDirection: 'column',
     height: '100%',
+    flexGrow: 1,
+    padding: 10,
   },
   input: {
     marginLeft: 5,
-  }
+  },
+  menuItem: {
+    display: 'flex',
+    width: '100%',
+    paddingBottom: 10,
+  },
 });
