0dd71d6935080dfc05d3ab3a5e77d31b4f3dbdb1
Carl Tablante <ctablante@seek.com.au>
2024-06-11 10:02:37 +1000

    fix: Codegen graphql

--- codegen.ts
+++ codegen.ts
@@ -3,17 +3,15 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "./schema/**/*.graphqls",
+  schema: "http://localhost:3000/api/query",
   documents: "src/**/*.tsx",
   generates: {
-    "src/gql/": {
+    "./src/__generated__/": {
       preset: "client",
       plugins: []
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+  },
+  ignoreNoDocuments: true,
 };
 
 export default config;
