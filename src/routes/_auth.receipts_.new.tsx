4e23b2885578ac92bcfda0432b9aba904c90310f
Carl Tablante <ctablante@seek.com.au>
2024-06-25 17:39:03 +1000

    feat: ReceiptsNewPage

--- src/routes/_auth.receipts_.new.tsx
+++ src/routes/_auth.receipts_.new.tsx
@@ -1,5 +1,14 @@
+import { Container, Title } from "@mantine/core"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_auth/receipts/new")({
-  component: () => <div>Hello /receipts/new!</div>,
+  component: ReceiptsNewPage,
 })
+
+function ReceiptsNewPage() {
+  return (
+    <Container>
+      <Title order={1}>New Receipt</Title>
+    </Container>
+  )
+}
