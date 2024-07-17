ab9396a93ad3078e809b25bff297f57f3d91fe0c
Carl Tablante <ctablante@seek.com.au>
2024-07-17 18:44:51 +1000

    feat: Added table for receipt items

--- src/routes/_auth.receipts/$receiptId.tsx
+++ src/routes/_auth.receipts/$receiptId.tsx
@@ -1,18 +1,31 @@
-import { Button, Container, Stack, Title } from '@mantine/core'
+import { Button, Container, Space, Stack, Table, TableData, Title } from '@mantine/core'
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { IconChevronLeft } from '@tabler/icons-react'
 
 const Receipt = () => {
+  const tableData: TableData = {
+    caption: 'Receipt line items',
+    head: ['Name', 'Price'],
+    body: [
+      ['Chickenjoy', '$5.00'],
+      ['Jolly Spaghetti', '$3.00'],
+      ['Peach Mango Pie', '$2.00'],
+    ],
+  }
+
   return (
     <Container>
       <Button component="a" leftSection={<IconChevronLeft />}>
         <Link to="/receipts">Back</Link>
       </Button>
+      <Space h="md" />
 
       <Stack>
         <Title order={1}>Jollibee</Title>
         <Title order={2}>Bida ang sarap</Title>
       </Stack>
+
+      <Table data={tableData} />
     </Container>
   )
 }
