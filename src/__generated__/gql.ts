bd2401774dacbf7659df8a0e942a6190bb975322
Carl Tablante <ctablante@seek.com.au>
2024-07-19 09:56:26 +1000

    feat: adding query

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -14,6 +14,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  */
 const documents = {
     "\n  query Me {\n    me {\n      id\n      username\n    }\n  }\n": types.MeDocument,
+    "\n  query Receipt($receiptId: ID!) {\n    receipt(id: $receiptId) {\n      id\n      total\n      items {\n        name\n        price\n      }\n    }\n  }\n": types.ReceiptDocument,
     "\n  mutation CreateMyReceipt($input: ReceiptInput) {\n    createMyReceipt(input: $input) {\n      id\n      total\n      description\n    }\n  }\n": types.CreateMyReceiptDocument,
     "\n  mutation DeleteMyReceipt($input: DeleteMyReceiptInput!) {\n    deleteMyReceipt(input: $input)\n  }\n": types.DeleteMyReceiptDocument,
     "\n  query MeWithReceipts {\n    me {\n      id\n      receipts {\n        id\n        description\n        total\n      }\n    }\n  }\n": types.MeWithReceiptsDocument,
@@ -40,6 +41,10 @@ export function graphql(source: string): unknown;
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  query Me {\n    me {\n      id\n      username\n    }\n  }\n"): (typeof documents)["\n  query Me {\n    me {\n      id\n      username\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  query Receipt($receiptId: ID!) {\n    receipt(id: $receiptId) {\n      id\n      total\n      items {\n        name\n        price\n      }\n    }\n  }\n"): (typeof documents)["\n  query Receipt($receiptId: ID!) {\n    receipt(id: $receiptId) {\n      id\n      total\n      items {\n        name\n        price\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/routes/_auth.receipts/$receiptId.tsx
+++ src/routes/_auth.receipts/$receiptId.tsx
@@ -1,6 +1,20 @@
 import { Button, Container, Space, Stack, Table, TableData, Title } from '@mantine/core'
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { IconChevronLeft } from '@tabler/icons-react'
+import { graphql } from '@src/__generated__/gql'
+
+const RECEIPT_QUERY = graphql(`
+  query Receipt($receiptId: ID!) {
+    receipt(id: $receiptId) {
+      id
+      total
+      items {
+        name
+        price
+      }
+    }
+  }
+`)
 
 const Receipt = () => {
   const tableData: TableData = {
@@ -15,14 +29,14 @@ const Receipt = () => {
 
   return (
     <Container>
-      <Button component="a" leftSection={<IconChevronLeft />}>
+      <Button leftSection={<IconChevronLeft />}>
         <Link to="/receipts">Back</Link>
       </Button>
       <Space h="md" />
 
       <Stack>
         <Title order={1}>Jollibee</Title>
-        <Title order={2}>Bida ang sarap</Title>
+        <Title order={2}>Price: $$$</Title>
       </Stack>
 
       <Table data={tableData} />
