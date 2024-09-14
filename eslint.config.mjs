7d82d74b4598ee592347f8cc2bc0928e0d14d0cb
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-09-14 18:34:30 +1000

    feat: Added @stylistic linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -18,5 +19,15 @@ export default tseslint.config(
 
   {
     ignores: ['dist/*', 'codegen.ts', 'eslint.config.mjs']
-  }
+  },
+
+  // stylistic rules
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/comma-dangle': 'error',
+    }
+  },
 );
